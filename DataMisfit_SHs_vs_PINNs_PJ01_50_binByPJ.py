f8339704d460891d40e3575a47f7d231b9541a9f
Leyuan Wu <88608914+LeyuanWu@users.noreply.github.com>
2024-04-27 18:02:36 +0800

    0427_1802

--- DataMisfit_SHs_vs_PINNs_PJ01_50_binByPJ.py
+++ DataMisfit_SHs_vs_PINNs_PJ01_50_binByPJ.py
@@ -1,6 +1,6 @@
 ##################################################################
 # # ! Juno Magnetic Modelling using <Vector Potential> with <3 NNs>
-# Plot showing the RMS errors of 'SH models' & `PINN Models` 
+# Table & Plot showing the RMS errors of 'SH models' & `PINN Models` 
 # computed at each perijove orbit of ***Juno Observation Orbits***
 ##################################################################
 # %%
@@ -11,21 +11,20 @@ from LW_DataReadWrite import *;
 # %%
 # # ! RMS of PINN Models Estimations of |B| at <Obs>: at individual orbit
 # TODO ******************** Parameters ******************** #
-cstRj=71492;      # Jupiter radius in km
-GS2nT=1e5;        # Gauss to nt
+cstRJ = 71492;      # Jupiter radius in km
+GS2nT = 1e5;        # Gauss to nt
+stsRJ = 4.0;          # Data for statistics (For comparison to other studies)
 # TODO ********************** end ************************* #
 fileObs='input/Juno_PJ01_50_4.0Rj.txt';
-nObs,PJ,Year,DD,xObs,yObs,zObs,bxObs,byObs,bzObs = LoadObsFile(fileObs,showinfo=False);
+nObs,PJ,Year,DD,xObs,yObs,zObs,BxObs,ByObs,BzObs = LoadObsFile(fileObs,showinfo=False);
+xObs=xObs/cstRJ; yObs=yObs/cstRJ; zObs=zObs/cstRJ;          # Distance in RJ
 RObs=np.sqrt(xObs**2+yObs**2+zObs**2); 
-RinRj=RObs/cstRj;
-bNormObs = np.sqrt(bxObs**2 + byObs**2 + bzObs**2);
+refBNorm = np.sqrt(BxObs**2 + ByObs**2 + BzObs**2);
 FILEs = ['output/JRM33_I30MDa_nmax18_EstBxyz_OurObs50.txt',\
-         'output/JRM33_I30MDa_nmax30_EstBxyz_OurObs50.txt',\
-         'output/Bloxham_I32MDb_nmax18_EstBxyz_OurObs50.txt',\
          'output/Bloxham_I32MDb_nmax32_EstBxyz_OurObs50.txt',\
          'output/JUNO_PINN_VP3_PJ01_33_4.0Rj_NN06_040_swish_Adam_DW1_RADk1c0n3000d600_nEpo0012000_BS0010000_Seed12345_EstBxyz_OurObs50.txt',\
          'output/JUNO_PINN_VP3_PJ01_50_4.0Rj_NN06_040_swish_Adam_DW1_RADk1c0n3000d600_nEpo0012000_BS0010000_Seed12345_EstBxyz_OurObs50.txt',];
-MODELs = ['JRM33 (n=18)','JRM33 (n=30)','Baseline (n=18)','Baseline (n=32)','PINN33e','PINN50e'];
+MODELs = ['JRM33 (n=18)','Baseline (n=32)','PINN33e','PINN50e'];
 
 nMODEL = len(MODELs); 
 pjs = np.unique(PJ); npj = pjs.size;
@@ -42,20 +41,21 @@ for iMODEL in range(nMODEL):
      print('RMS of %s evaluated at Juno <OBS>:'%(modelName),flush=True);
      for ipj in range(npj):
           curpj = pjs[ipj];
-          pkInd = np.logical_and((PJ==curpj),(RinRj<=4.0));
-          refBx_pj = bxObs[pkInd]; refBy_pj = byObs[pkInd]; refBz_pj = bzObs[pkInd];
-          refBNorm_pj = bNormObs[pkInd];
+          pkInd = np.logical_and((PJ==curpj),(RObs<=stsRJ));
+          refBx_pj = BxObs[pkInd]; refBy_pj = ByObs[pkInd]; refBz_pj = BzObs[pkInd];
+          refBNorm_pj = refBNorm[pkInd];
           estBx_pj = estBx[pkInd]; estBy_pj = estBy[pkInd]; estBz_pj = estBz[pkInd];
           estBNorm_pj = estBNorm[pkInd];
-          # Delta |B|
-          errBNorm1 = estBNorm_pj - refBNorm_pj;
-          # |Delta B|
-          errBNorm2 = np.sqrt((estBx_pj-refBx_pj)**2+(estBy_pj-refBy_pj)**2+(estBz_pj-refBz_pj)**2);
-          # Delta Bx, By, Bz
-          err_dBxyz = np.concatenate((estBx_pj-refBx_pj,estBy_pj-refBy_pj,estBz_pj-refBz_pj),axis=0);
-          print('PJ[%02d], NOB: %4d, '%(curpj,np.count_nonzero(pkInd)),end='',flush=True);
+          err_dB = estBNorm_pj - refBNorm_pj;   # Delta |B|
+          err_DB = np.sqrt((estBx_pj-refBx_pj)**2+(estBy_pj-refBy_pj)**2+(estBz_pj-refBz_pj)**2);   # |Delta B|
+          err_dBxyz = np.concatenate((estBx_pj-refBx_pj,estBy_pj-refBy_pj,estBz_pj-refBz_pj),axis=0);  # Delta Bx, By, Bz
+          # rms = np.sqrt(np.mean(err_dB**2));
+          # rms = np.sqrt(np.mean(err_DB**2));
           rms = np.sqrt(np.mean(err_dBxyz**2));
-          e2 = rms/np.sqrt(np.mean(refBNorm_pj**2));
+          print('PJ[%02d], NOB: %4d, '%(curpj,np.count_nonzero(pkInd)),end='',flush=True);
+          refBxyz_pj = np.concatenate((refBx_pj,refBy_pj,refBz_pj),axis=0);
+          refRMS = np.sqrt(np.mean(refBxyz_pj**2))
+          e2 = rms/refRMS; # e2 = rms/np.sqrt(np.mean(refBNorm_pj**2)) for the other two
           print('rms = %8.1f nT; E2 = %8.4f %%'%(GS2nT*rms,100*e2),flush=True);
           RMS[iMODEL].append(GS2nT*rms);
           E2[iMODEL].append(100*e2);
@@ -63,21 +63,20 @@ for iMODEL in range(nMODEL):
      print('',flush=True); # <br>
 # %%
 # # ! Plot of PINN Models Estimations Error of |B| at <Obs>: at individual orbit
-pkMODELS = np.array([1,4,5,6])-1;
-# pkMODELS = np.array([1,2,3,4,5,6])-1;
+pkMODELS = np.array([1,2,3,4])-1;
 fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 6), dpi=150, layout='constrained');
-yLables=['RMS error of $|\mathbf{B}|$ (nT)','$E_2$ of $|\mathbf{B}|$ (%)'];
-COLORs =['#00FFFF','C1','C2','#FF00FF','#FF0000','#0000FF'];
-LSs = ['-','-','-','-','-','-'];
-MARKERs =['o','o','o','o','o','o'];
+yLables=['RMS error of $\mathbf{B}$ (nT)','$E_2$ of $\mathbf{B}$ (%)'];
+COLORs =['#00FFFF','#FF00FF','#FF0000','#0000FF'];
+LSs = ['-','-','-','-'];
+MARKERs =['o','o','o','o'];
 for iMODEL in pkMODELS:
      modelName = MODELs[iMODEL];
      axs[0].step(pjs,RMS[iMODEL],color=COLORs[iMODEL],linestyle=LSs[iMODEL],where='mid',label=modelName);
      axs[0].plot(pjs,RMS[iMODEL], MARKERs[iMODEL], color=COLORs[iMODEL],linestyle='',alpha=0.5);
-     axs[0].set_ylim(bottom=0,top=3200);
+     axs[0].set_ylim(bottom=0,top=3000);
      axs[1].step(pjs,E2[iMODEL], color=COLORs[iMODEL],linestyle=LSs[iMODEL],where='mid',label=modelName);
      axs[1].plot(pjs,E2[iMODEL],  MARKERs[iMODEL], color=COLORs[iMODEL],linestyle='',alpha=0.5);
-     axs[1].set_ylim(bottom=0,top=1.2);
+     axs[1].set_ylim(bottom=0,top=2);
 for iax in range(len(axs)):
      axs[iax].axvline(33.5,color='black',linestyle='--');
      axs[iax].set_xticks(pjs);
@@ -87,5 +86,5 @@ for iax in range(len(axs)):
      axs[iax].set_ylabel(yLables[iax]);
      axs[iax].legend(loc='best', ncol = pkMODELS.size//2)  # Add a legend
      axs[iax].tick_params(axis='x',which='major',labelrotation=45,labelsize='small');
-plt.savefig('NN06_040_Plot_DataMisfit_SHs_vs_PINNs_OurObs50_binByPJ.png',dpi=300);
+plt.savefig('Fig2.png',dpi=300);
 plt.show();
